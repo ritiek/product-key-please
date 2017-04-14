8d2010f8f0fb8c6871e10dab33513adcc7b7215f
Ritiek Malhotra <Ritiek@users.noreply.github.com>
2017-04-14 13:34:41 +0530

    Switch to google from duckduckgo to search more than one page

--- find_key.py
+++ find_key.py
@@ -31,23 +31,34 @@ template = sys.argv[2]
 regex_compile = reCompiler(template)
 pattern = re.compile(regex_compile)
 
-page = requests.get('https://duckduckgo.com/html/?q=' +  query + ' product key').text
-soup = BeautifulSoup(page, 'html.parser')
-links = soup.find_all('a', {'class':'result__a'})
+number = 0      # 0 = page 1. 10 = page 2. ...
 
-for x in links:
-	inner_link = 'https://duckduckgo.com' + x['href']
-	page = requests.get(inner_link).text
+while True:
+	page = requests.get('https://www.google.com/search?q=' + query.replace(' ', '+') + '+product+key&start=' + str(number)).text
 	soup = BeautifulSoup(page, 'html.parser')
-	link = (soup.find('script').get_text()).replace('window.parent.location.replace("', '').replace('");', '')
-	if not link[-4:] == '.pdf':
-		print(link)
-		chance_html = requests.get(link).text
-		chance_text = BeautifulSoup(chance_html, 'html.parser').find('body').get_text()
-		#print chance_html
-		print(pattern.findall(chance_text))
-		print('')
-		file = query.replace(' ', '_') + '.txt'
-		with open(('keys/' + file), 'a') as key_file:
-			for y in pattern.findall(chance_text):
-				key_file.write(y + '\n')
+	#print soup
+	#print 'searching'
+	#print soup.find_all('h3', {'class':'r'})
+	try:
+		for x in soup.find_all('h3', {'class':'r'}):
+			raw_link = x.find('a')['href']
+			end = raw_link.find('&sa=')
+			#print 'checking validity'
+			if raw_link[7:end].find('?q=') == -1:
+				link = raw_link[7:end]
+				if not link[-4:] == '.pdf':
+					print(link)
+					chance_html = requests.get(link).text
+					chance_text = BeautifulSoup(chance_html, 'html.parser').find('body').get_text()
+					#print chance_html
+					print(pattern.findall(chance_text))
+					print('')
+					file = query.replace(' ', '_') + '.txt'
+					with open(('keys/' + file), 'a') as key_file:
+						for y in pattern.findall(chance_text):
+							key_file.write(y + '\n')
+	except KeyboardInterrupt:
+		exit()
+	except:
+		pass
+	number += 10
